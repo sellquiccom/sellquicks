3cf373df0cdd53f9ce964d14856f608e1ca5f24e
Maurice <sellquic.com@gmail.com>
2025-10-02 07:21:55 +0000

    now help sove same issue for another website: Access to XMLHttpRequest a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDWH9xun1AvzZFWQOH1GqM2t1VyzRz75FE",
   authDomain: "sellquic.firebaseapp.com",
   projectId: "sellquic",
-  storageBucket: "sellquic.appspot.com",
+  storageBucket: "sellquic.firebasestorage.app",
   messagingSenderId: "162539089525",
   appId: "1:162539089525:web:5b6566781d6e5a97c7123c"
 };
