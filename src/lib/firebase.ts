fa3a6ec8b6d5b047417efa19e6e274da16fb2ae2
Maurice <sellquic.com@gmail.com>
2025-10-02 12:48:54 +0000

    updates this in the env:  apiKey: "AIzaSyDWH9xun1AvzZFWQOH1GqM2t1VyzRz75

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDWH9xun1AvzZFWQOH1GqM2t1VyzRz75FE",
   authDomain: "sellquic.firebaseapp.com",
   projectId: "sellquic",
-  storageBucket: "sellquic.firebasestorage.app",
+  storageBucket: "sellquic.appspot.com",
   messagingSenderId: "162539089525",
   appId: "1:162539089525:web:5b6566781d6e5a97c7123c"
 };
