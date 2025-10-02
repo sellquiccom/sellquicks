55fc9c3659f736203698ccf8bad716960ec44001
Maurice <sellquic.com@gmail.com>
2025-10-02 06:39:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-435970349-5f36f",
+  "appId": "1:546553568459:web:7df42d2dfbab65b46b4f4e",
+  "apiKey": "AIzaSyCzdVYBdbP44n89zOtUwv-_jekpaRLOjfU",
+  "authDomain": "studio-435970349-5f36f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "546553568459"
+};
